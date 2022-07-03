50bd80b3b1df659db085d27119e6c654c5c9a519
scl67ooo <103156500+scl67ooo@users.noreply.github.com>
2022-07-03 23:53:26 +0900

    [Bronze III] Title: 오븐 시계, Time: 216 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,22 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int h = sc.nextInt();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
 		int m = sc.nextInt();
 		int cook = Integer.parseInt(sc.next());
-		
-		m += cook;
-		while (m >= 60) {
-			m -= 60;
-			if (h == 23) {
-				h -= 23;
-			} else {
-				h++;
-			}
+
+		h += ((m + cook) / 60);
+		if ((h / 24) > 0) {
+			h %= 24;
 		}
-        System.out.println(h+" "+m);
-        sc.close();
-    }
+		m = ((m + cook) % 60);
+		System.out.println(h + " " + m);
+		sc.close();
+	}
 }
