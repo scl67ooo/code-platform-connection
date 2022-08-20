b30df88172e16acc41c6bd5cbaae8bf5af0d8472
scl67ooo <103156500+scl67ooo@users.noreply.github.com>
2022-08-20 22:58:53 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 356 ms, Memory: 23120 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int row = Integer.parseInt(sc.next());
+		for (int i = 1; i <= row; i++) {
+			for (int k = row - i; k >= 1; k--) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
