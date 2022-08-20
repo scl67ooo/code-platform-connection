8d0b9725f9acac4aa7adddf3fded34886a820841
scl67ooo <103156500+scl67ooo@users.noreply.github.com>
2022-08-20 22:49:37 +0900

    [Bronze V] Title: A+B - 8, Time: 216 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int cases = Integer.parseInt(sc.next());
+		int[] a = new int[cases];
+		int[] b = new int[cases];
+		for (int i = 0; i < cases; i++) {
+			a[i] = Integer.parseInt(sc.next());
+			b[i] = Integer.parseInt(sc.next());
+			System.out.println("Case #"+(i+1)+": "+a[i]+" + "+b[i]+" = "+(a[i]+b[i]));
+		}
+	}
+}
