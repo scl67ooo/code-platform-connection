3203e602f3df74caa3203f89572cfa1ffe1a5f8e
scl67ooo <103156500+scl67ooo@users.noreply.github.com>
2022-08-26 16:34:03 +0900

    [Silver V] Title: 달팽이는 올라가고 싶다, Time: 120 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Silver/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -2,15 +2,16 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
         BufferedWriter bw = new BufferedWriter( new OutputStreamWriter( System.out ) );
-        String[] abv = br.readLine().split(" "); 
-		int a = Integer.parseInt(abv[0]);
-        int b = Integer.parseInt(abv[1]);
-        int v = Integer.parseInt(abv[2]);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+       	int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
 		int result = (int) Math.ceil((double)(v-b)/(double)(a-b));
 		bw.write(result+"\n");
 		bw.flush();
